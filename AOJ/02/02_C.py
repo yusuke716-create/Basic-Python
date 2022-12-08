03a5bdb11249e1cec38f5a2f1a72de02f8988468
yusuke716-create <you.150716@gmail.com>
2022-12-08 22:55:04 +0900

    "再提出"

--- AOJ/02/02_C.py
+++ AOJ/02/02_C.py
@@ -1,18 +1,4 @@
-a, b, c = map(int, input().split())
-if a <= b <= c:
-    print(a, b, c)
-elif a <= c <= b:
-    a, b, c = a, c, b
-    print(a, b, c)
-elif b <= a <= c:
-    a, b, c = b, a, c
-    print(a, b, c)
-elif b <= c <= a:
-    a, b, c = b, c, a
-    print(a, b, c)
-elif c <= a <= b:
-    a, b, c = c, a, b
-    print(a, b, c)
-else:
-    a, b, c = c, b, a
-    print(a, b, c)
+l = list(map(int, input().split()))
+l.sort()
+a, b, c = l
+print(a,b, c)
